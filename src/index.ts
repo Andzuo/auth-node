4a659dd19d99b119f177b92e37cb824d554e7c0c
Andzuo <alucasm02@gmail.com>
2024-06-29 01:28:21 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from './data-source';
+import express from 'express';
+import router from './router';
+import { errorMiddleware } from './middlewares/error';
+
+AppDataSource.initialize().then(() => {
+    const app = express();
+
+    app.use(express.json());
+
+    app.use(router)
+
+    app.use(errorMiddleware);
+
+    return app.listen(process.env.PORT)
+});
