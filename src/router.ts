23ff9fab160d411d9cf87d98801808624800ce3b
Andzuo <alucasm02@gmail.com>
2024-06-30 09:38:11 -0300

    authMiddleware and routes

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import 'express-async-errors';
 import { LoginController } from './controllers/loginController';
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { authMiddleware } from './middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/user', new UserController().create);
 router.post('/login', new LoginController().login);
+router.get('/profile', authMiddleware, new UserController().getProfile);
 
 export default router;
